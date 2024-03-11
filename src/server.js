421e34b10c7ec854971c301c56c9c8de620655a4
Minh Huy <tmh.pug@gmail.com>
2024-03-11 22:17:54 +0700

    Update connetion to DB

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoute from "./routes/web";
+import connectDB from "./config/connectDB";
 
 require("dotenv").config();
 
@@ -10,6 +11,8 @@ let app = express();
 // config
 viewEngine(app);
 initWebRoute(app);
+
+connectDB();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
