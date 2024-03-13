7df7d7aa2b6f5c6bbb26e1e77d4630afa7578b19
Minh Huy <tmh.pug@gmail.com>
2024-03-13 23:23:25 +0700

    Create migration, seeder, model for database

---
+++ src/seeders/seeder-user.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Users", [
+      {
+        email: "admin@gmail.com",
+        password: "123456",
+        firstName: "Jayden",
+        lastName: "Truong",
+        address: "1234 Phan Van Tri, Go Vap, Ho Chi Minh",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Users", null, {});
+  },
+};
